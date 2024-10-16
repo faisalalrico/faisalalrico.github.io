c889f29a0e14945bd83fbdeef064aeafdf1a22cb
Faisal Alrico <if23.faisalalrico@mhs.ubpkarawang.ac.id>
2024-10-16 15:31:36 +0700

    Delete cp-script.js

--- cp-script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("hamburger").onclick = function() {
-    var navLinks = document.getElementById("nav-links");
-    navLinks.classList.toggle("show"); // Mengubah kelas untuk menampilkan/menghilangkan tautan
-};
